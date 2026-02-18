d2d4bb48cec9a4927112f01fcc8e523afce8c082
Kae Loran <kae.loran@prks.com.br>
2026-02-18 09:31:20 -0300

    fix(CI): Export db variables para test

--- database/db.go
+++ database/db.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
@@ -14,7 +16,24 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+	// allow overriding connection parameters via environment variables so tests
+	// and other environments can point to the appropriate database host.
+	// The default is "localhost" since most development and CI (where the
+	// postgres container is published on 127.0.0.1) connect from the host
+	// machine. When running the application inside a Docker network you can set
+	// DB_HOST=postgres (or whatever service name) in the container's env
+	// configuration.
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
