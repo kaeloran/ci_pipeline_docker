add5beac5efe58802eb24c52764972dc857625e0
Kae Loran <kae.loran@prks.com.br>
2026-02-18 13:48:04 -0300

    feat(ci): Ajustado para rodar local

--- database/db.go
+++ database/db.go
@@ -15,12 +15,27 @@ var (
 	err error
 )
 
-func ConectaComBancoDeDados() {	
+func ConectaComBancoDeDados() {
 	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
 	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "root"
+	}
 	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "root"
+	}
 	dbname := os.Getenv("DB_NAME")
+	if dbname == "" {
+		dbname = "root"
+	}
 	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
 
 	stringDeConexao := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
