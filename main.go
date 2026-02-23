99923f51460f63f64c54f89b1622e2f0cc7dd082
Kae Loran <kae.loran@prks.com.br>
2026-02-23 14:33:06 -0300

    Deploy EC2 via ssm

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"ci_pipeline_docker/database"
 	"ci_pipeline_docker/routes"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar arquivo .env")
+	}
+
 	database.ConectaComBancoDeDados()
 	routes.HandleRequest()
 }
