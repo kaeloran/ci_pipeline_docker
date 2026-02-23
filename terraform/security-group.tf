1718e125b3000e058ccbd524b600720cbff7077d
Kae Loran <kae.loran@prks.com.br>
2026-02-23 11:51:15 -0300

    chore(Deploy): :up: terraform, deploy via ssm

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "asg-alura-go-api-dev" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
   tags = {
     Name = "app"
