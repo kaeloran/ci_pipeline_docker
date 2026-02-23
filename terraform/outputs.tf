f21838c3907375b83347eb188b197de5443a0499
Kae Loran <kae.loran@prks.com.br>
2026-02-23 13:53:43 -0300

    fix(CI): Correções secrets database

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,18 +8,6 @@ output "db_port" {
   value       = aws_db_instance.postgres-alura-go-dev.port
 }
 
-output "db_user" {
-  description = "Database username"
-  value       = aws_db_instance.postgres-alura-go-dev.username
-  sensitive   = true
-}
-
-output "db_password" {
-  description = "Database password"
-  value       = aws_db_instance.postgres-alura-go-dev.password
-  sensitive   = true
-}
-
 output "ec2_instance_id" {
   description = "ID da Instância EC2 (Necessário para o AWS SSM)"
   value       = aws_instance.alura-go-api-dev.id
